e50bffa1c1c1d276b2b5b35612ba7a26e0074668
James Christensen <jchriste@jcvi.org>
2018-04-05 16:17:13 -0400

    added ServiceException

---
+++ src/main/java/org/jcvi/vigor/service/exception/ServiceException.java
@@ -0,0 +1,19 @@
+package org.jcvi.vigor.service.exception;
+
+import org.jcvi.vigor.exception.VigorException;
+
+public class ServiceException extends VigorException {
+
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ServiceException(Throwable cause) {
+        super(cause);
+    }
+
+}
