8ef0942105dff83bf9d4832d7c4f5eef5f27c649
James Christensen <jchriste@jcvi.org>
2018-04-06 15:50:13 -0400

    no exception thrown

--- src/main/java/org/jcvi/vigor/Application.java
+++ src/main/java/org/jcvi/vigor/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("org.jcvi.vigor")
 public class Application {
 
-	public static void main(String... args) throws Exception {
+	public static void main(String... args)  {
     	try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Application.class); ) {
 			ctx.getBean(Vigor.class).run(args);
 		}
