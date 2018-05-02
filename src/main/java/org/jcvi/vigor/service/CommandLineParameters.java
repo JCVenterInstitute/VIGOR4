e21d3c4bd82aa164acfd51a9cfdc8767e5bfc246
James Christensen <jchriste@jcvi.org>
2018-05-02 15:36:13 -0400

    normalize commandline parameters to use underscore

--- src/main/java/org/jcvi/vigor/service/CommandLineParameters.java
+++ src/main/java/org/jcvi/vigor/service/CommandLineParameters.java
@@ -6,17 +6,17 @@ public final class CommandLineParameters {
 	public final static String referenceDB = "reference_database";
 	public final static String genbankDB = "genbank_reference";
 	public final static String eValue = "evalue";
-	public final static String minCoverage = "min-coverage";
+	public final static String minCoverage = "min_coverage";
 
 	public final static String circularGene = "circular_gene";
-	public final static String completeGene = "completeGene";
+	public final static String completeGene = "complete_gene";
 	public final static String frameshiftSensitivity = "frameshift_sensitivity";
 	public final static String skipSelection = "skip_selection";
 	public final static String useLocusTags = "use_locus_tags";
 	public final static String jcviRules = "jcvi_rules";
 	public final static String ignoreReferenceRequirements = "ignore_reference_requirements";
 	public final static String parameters = "parameters";
-	public final static String minGeneSize = "mine_gene_size";
+	public final static String minGeneSize = "min_gene_size";
 	public final static String verbose = "verbose";
 	public final static String ignoreRefID = "ignore_refID";
 	public final static String listConfigParameters = "list_config_parameters";
