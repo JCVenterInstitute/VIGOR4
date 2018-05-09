131a9bee0a6005449f06fb04c6aa1db1b2c13b3c
James Christensen <jchriste@jcvi.org>
2018-05-09 09:23:56 -0400

    dynamically skip adjustRNAEditingTest when mmp_db is not available

--- src/test/java/org/jcvi/vigor/service/AdjustViralTricksTest.java
+++ src/test/java/org/jcvi/vigor/service/AdjustViralTricksTest.java
@@ -4,18 +4,17 @@ package org.jcvi.vigor.service;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.jcvi.vigor.exception.VigorException;
-import org.jcvi.vigor.service.exception.ServiceException;
 import org.jcvi.vigor.utils.ConfigurationParameters;
 import org.jcvi.vigor.utils.VigorConfiguration;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.jcvi.jillion.core.Range;
@@ -24,7 +23,6 @@ import org.jcvi.vigor.component.Alignment;
 import org.jcvi.vigor.component.Model;
 import org.jcvi.vigor.forms.VigorForm;
 import org.jcvi.vigor.utils.VigorTestUtils;
-import org.jcvi.vigor.utils.VigorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -97,19 +95,19 @@ public class AdjustViralTricksTest {
     }
 
 
-    @Ignore("mmp database not yet validated for vigor 4")
     @Test
     public void adjustRNAEditingTest() throws VigorException, CloneNotSupportedException{
 
         VigorConfiguration config = initializationService.mergeConfigurations(initializationService.getDefaultConfigurations());
         String referenceDBPath = config.get(ConfigurationParameters.ReferenceDatabasePath);
         assertThat("reference database path must be set", referenceDBPath, is(notNullValue()));
-        String referenceDB = Paths.get(referenceDBPath, "mmp_db").toString();
+        Path referenceDB = Paths.get(referenceDBPath, "mmp_db");
+        assumeTrue("This test requires the mmp_db", referenceDB.toFile().exists());
         List<Alignment> alignments;
         List<Model> models=new ArrayList<Model>();
         File virusGenomeSeqFile = new File(classLoader.getResource("vigorUnitTestInput/mmp_rna_editing_Test.fasta"). getFile());
         File alignmentOutput = new File(classLoader.getResource("vigorUnitTestInput/mmp_rna_editing_Test.txt"). getFile());
-        alignments = VigorTestUtils.getAlignments(virusGenomeSeqFile,referenceDB,alignmentOutput,config);
+        alignments = VigorTestUtils.getAlignments(virusGenomeSeqFile,referenceDB.toString(),alignmentOutput,config);
         for (int i=0; i<alignments.size(); i++) {
             alignments.set(i, viralProteinService.setViralProteinAttributes(alignments.get(i), new VigorForm(config)));
         }
