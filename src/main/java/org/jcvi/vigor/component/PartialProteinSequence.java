57aaeb07f3df41e54b7559fa665c2babbc8dee81
James Christensen <jchriste@jcvi.org>
2018-08-01 14:50:14 -0400

    add proteinID to PartialProteinSequence

--- src/main/java/org/jcvi/vigor/component/PartialProteinSequence.java
+++ src/main/java/org/jcvi/vigor/component/PartialProteinSequence.java
@@ -4,18 +4,21 @@ package org.jcvi.vigor.component;
 import org.jcvi.jillion.core.residue.aa.ProteinSequence;
 
 public class PartialProteinSequence {
+
     final boolean partial3p;
     final boolean partial5p;
     final ProteinSequence sequence;
+    final String proteinID;
 
-    private PartialProteinSequence(ProteinSequence sequence, boolean partial3p, boolean partial5p) {
+    private PartialProteinSequence(String proteinID, ProteinSequence sequence, boolean partial3p, boolean partial5p) {
+        this.proteinID = proteinID;
         this.sequence = sequence;
         this.partial3p = partial3p;
         this.partial5p = partial5p;
     }
 
-    public static PartialProteinSequence of (ProteinSequence sequence, boolean partial3p, boolean partial5p) {
-        return new PartialProteinSequence(sequence, partial3p, partial5p);
+    public static PartialProteinSequence of (String proteinID, ProteinSequence sequence, boolean partial3p, boolean partial5p) {
+        return new PartialProteinSequence(proteinID, sequence, partial3p, partial5p);
     }
 
     public boolean isPartial3p() {
@@ -28,5 +31,9 @@ public class PartialProteinSequence {
 
     public ProteinSequence getSequence() {
         return sequence;
+    }
+
+    public String getProteinID() {
+        return proteinID;
     }
 }
