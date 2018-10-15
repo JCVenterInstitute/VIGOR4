b2a7aee05af97fe9a31b3b4c436fd43b96d0b164
James Christensen <jchriste@jcvi.org>
2018-10-15 16:27:01 -0400

    VIG-20. Coordinates are always smallest to largest regardless of strand

--- src/main/java/org/jcvi/vigor/utils/GenerateGFF3Output.java
+++ src/main/java/org/jcvi/vigor/utils/GenerateGFF3Output.java
@@ -29,10 +29,10 @@ public class GenerateGFF3Output {
             String geneomeSeqID = virusGenome.getId();
             List<Exon> exons = geneModel.getExons();
             String geneName = geneModel.getAlignment().getViralProtein().getGeneSymbol();
-            String CDSStart =Long.toString(VigorFunctionalUtils.getDirectionBasedCoordinate
-                    (exons.get(0).getRange().getBegin(oneBased),seqlength,geneModel.getDirection()));
-            String CDSEnd = Long.toString(VigorFunctionalUtils.getDirectionBasedCoordinate
-                    (exons.get(exons.size() - 1).getRange().getEnd(oneBased),seqlength,geneModel.getDirection()));
+            long CDSStart =VigorFunctionalUtils.getDirectionBasedCoordinate
+                    (exons.get(0).getRange().getBegin(oneBased),seqlength,geneModel.getDirection());
+            long CDSEnd = VigorFunctionalUtils.getDirectionBasedCoordinate
+                    (exons.get(exons.size() - 1).getRange().getEnd(oneBased),seqlength,geneModel.getDirection());
             String mRnaID = geneModel.getGeneID() + "." + i;
             bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
             //gene
@@ -41,7 +41,7 @@ public class GenerateGFF3Output {
             } else {
                 bw.write("gene" + "\t");
             }
-            bw.write(CDSStart + "\t" + CDSEnd + "\t");
+            bw.write(Math.min(CDSStart,CDSEnd) + "\t" + Math.max(CDSStart,CDSEnd) + "\t");
             bw.write("." + "\t");
             if (geneModel.getDirection().equals(Direction.FORWARD)) {
                 bw.write("+" + "\t");
@@ -56,7 +56,7 @@ public class GenerateGFF3Output {
             //mRNA
             bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
             bw.write("mRNA" + "\t");
-            bw.write(CDSStart + "\t" + CDSEnd + "\t");
+            bw.write(Math.min(CDSStart,CDSEnd) + "\t" + Math.max(CDSStart,CDSEnd) + "\t");
             bw.write("." + "\t");
             if (geneModel.getDirection().equals(Direction.FORWARD)) {
                 bw.write("+" + "\t");
@@ -67,26 +67,17 @@ public class GenerateGFF3Output {
                 bw.write("Partial;");
             }
             bw.write("\n");
-            //remark
-            /*bw.write(geneomeSeqID+"\t"+"vigor"+"\t");
-            bw.write("remark"+"\t");
-            bw.write(".\t.\t.\t.\t.\t");
-            bw.write("Parent="+mRnaID+";");
-            bw.write("Note={};");
-            bw.write("\n");*/
             //exon
             IDGenerator idGenerator = new IDGenerator(mRnaID);
             for (int j = 0; j < exons.size(); j++) {
                 Exon exon = exons.get(j);
                 bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
                 bw.write("exon" + "\t");
-                String exonBegin =Long.toString(VigorFunctionalUtils.getDirectionBasedCoordinate(exon.getRange().getBegin(oneBased),seqlength,geneModel.getDirection()));
-                String exonEnd =Long.toString(VigorFunctionalUtils.getDirectionBasedCoordinate(exon.getRange().getEnd(oneBased),seqlength,geneModel.getDirection()));
-                bw.write(exonBegin + "\t" + exonEnd + "\t");
+                long exonBegin =VigorFunctionalUtils.getDirectionBasedCoordinate(exon.getRange().getBegin(oneBased),seqlength,geneModel.getDirection());
+                long exonEnd = VigorFunctionalUtils.getDirectionBasedCoordinate(exon.getRange().getEnd(oneBased),seqlength,geneModel.getDirection());
+                bw.write(Math.min(exonBegin, exonEnd) + "\t" + Math.max(exonBegin, exonEnd) + "\t");
                 bw.write("." + "\t");
-                if (geneModel.getDirection().equals(Direction.FORWARD)) {
-                    bw.write("+" + "\t");
-                } else bw.write("-" + "\t");
+                bw.write((geneModel.getDirection() == Direction.FORWARD ? "+" : '-') + "\t");
                 bw.write(exon.getFrame().getFrame() - 1 + "\t");
                 bw.write(String.format("ID=%s;Parent=%s;", idGenerator.next(), mRnaID));
                 if (( j == 0 && geneModel.isPartial5p() ) || ( j == exons.size() - 1 && geneModel.isPartial3p() )) {
@@ -97,7 +88,7 @@ public class GenerateGFF3Output {
             //CDS
             bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
             bw.write("CDS" + "\t");
-            bw.write(CDSStart + "\t" + CDSEnd + "\t");
+            bw.write(Math.min(CDSStart,CDSEnd) + "\t" + Math.max(CDSStart,CDSEnd) + "\t");
             bw.write("." + "\t");
             if (geneModel.getDirection().equals(Direction.FORWARD)) {
                 bw.write("+" + "\t");
@@ -112,9 +103,9 @@ public class GenerateGFF3Output {
             if (geneModel.getInsertRNAEditingRange() != null) {
                 bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
                 bw.write("insertion" + "\t");
-                String insertBegin= Long.toString(VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getInsertRNAEditingRange().getBegin(oneBased),seqlength,geneModel.getDirection()));
-                String insertEnd=Long.toString(VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getInsertRNAEditingRange().getEnd(oneBased),seqlength,geneModel.getDirection()));
-                bw.write(insertBegin + "\t" + insertEnd + "\t");
+                long insertBegin= VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getInsertRNAEditingRange().getBegin(oneBased),seqlength,geneModel.getDirection());
+                long insertEnd=VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getInsertRNAEditingRange().getEnd(oneBased),seqlength,geneModel.getDirection());
+                bw.write(Math.min(insertBegin, insertEnd)+ "\t" + Math.min(insertBegin,insertEnd) + "\t");
                 bw.write("." + "\t");
                 if (geneModel.getDirection().equals(Direction.FORWARD)) {
                     bw.write("+" + "\t");
@@ -130,7 +121,7 @@ public class GenerateGFF3Output {
                 bw.write("stop_codon_read_through" + "\t");
                 long replaceStopBegin= VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getReplaceStopCodonRange().getBegin(oneBased),seqlength,geneModel.getDirection());
                 long replaceStopEnd=VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getReplaceStopCodonRange().getEnd(oneBased),seqlength,geneModel.getDirection());
-                bw.write(Long.toString(replaceStopBegin) + "\t" + Long.toString(replaceStopEnd) + "\t");
+                bw.write(Math.min(replaceStopBegin, replaceStopEnd) + "\t" + Math.max(replaceStopBegin,replaceStopEnd) + "\t");
                 bw.write("." + "\t");
                 if (geneModel.getDirection().equals(Direction.FORWARD)) {
                     bw.write("+" + "\t");
@@ -141,36 +132,21 @@ public class GenerateGFF3Output {
                     bw.write(String.format("Note=%s;", ( NoteType.StopCodonReadThrough )));
                 bw.write("\n");
             }
-            //minus_1_translationally_frameshifted
+            //plus/minus_1_translationally_frameshifted
             if (geneModel.getAlignment().getViralProtein().getGeneAttributes().getRibosomal_slippage().isHas_ribosomal_slippage() && geneModel.getRibosomalSlippageRange() != null) {
                 int frameshift = geneModel.getAlignment().getViralProtein().getGeneAttributes().getRibosomal_slippage().getSlippage_frameshift();
                 long slippageBegin = VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getRibosomalSlippageRange().getBegin(oneBased),seqlength,geneModel.getDirection());
                 long slippageEnd = VigorFunctionalUtils.getDirectionBasedCoordinate(geneModel.getRibosomalSlippageRange().getEnd(oneBased),seqlength,geneModel.getDirection());
-                if (frameshift == -1) {
+                if (frameshift == -1 || frameshift == 1) {
                     bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
-                    bw.write("mRNA_with_minus_1_frameshift" + "\t");
-                    bw.write(Long.toString(slippageBegin)+ "\t" + Long.toString(slippageEnd) + "\t");
+                    bw.write(frameshift == -1 ? "mRNA_with_minus_1_frameshift": "mRNA_with_plus_1_frameshift" + "\t");
+                    bw.write(Math.min(slippageBegin, slippageEnd)+ "\t" + Math.max(slippageBegin,slippageEnd) + "\t");
                     bw.write("." + "\t");
                     if (geneModel.getDirection().equals(Direction.FORWARD)) {
                         bw.write("+" + "\t");
                     } else bw.write("-" + "\t");
                     bw.write("." + "\t");
                     bw.write(String.format("ID=%s;Parent=%s;", idGenerator.next(), mRnaID));
-                    //bw.write("Note={};");
-                    bw.write("\n");
-                }
-                //plus_1_translationally_frameshifted
-                if (frameshift == 1) {
-                    bw.write(geneomeSeqID + "\t" + "vigor" + "\t");
-                    bw.write("mRNA_with_plus_1_frameshift" + "\t");
-                    bw.write(Long.toString(slippageBegin)+ "\t" + Long.toString(slippageEnd) + "\t");
-                    bw.write("." + "\t");
-                    if (geneModel.getDirection().equals(Direction.FORWARD)) {
-                        bw.write("+" + "\t");
-                    } else bw.write("-" + "\t");
-                    bw.write("." + "\t");
-                    bw.write(String.format("ID=%s;Parent=%s;", idGenerator.next(), mRnaID));
-                    // bw.write("Note={};");
                     bw.write("\n");
                 }
             }
