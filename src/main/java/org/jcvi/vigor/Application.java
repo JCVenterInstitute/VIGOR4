43f631438c6f41cca2dc9555b1e8cc466cd5ea5b
James Christensen <jchriste@jcvi.org>
2019-03-22 14:59:52 -0400

    remove unused @Configuration annotation

--- src/main/java/org/jcvi/vigor/Application.java
+++ src/main/java/org/jcvi/vigor/Application.java
@@ -2,9 +2,7 @@ package org.jcvi.vigor;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ComponentScan("org.jcvi.vigor")
 public class Application {
 
